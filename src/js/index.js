6977b0d85f67d3addf8e7ee99480fcbd36d1e10e
Moises <moises.gonzalez@flixbus.com>
2024-11-22 17:24:15 +0100

    Adds all functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,25 @@
 const display = document.getElementById("display");
 
+// Addition function
+function add(a, b) {
+  return a + b;
+}
 
-// TODOO
-// // Addition function
-// function add(a, b) {
-//   return a + b;
-// }
+// Subtraction function
+function subtract(a, b) {
+  return a - b;
+}
 
-// // Subtraction function
-// function subtract(a, b) {
-//   return a - b;
-// }
+// Multiplication function
+function multiply(a, b) {
+  return a * b;
+}
 
-// // Multiplication function
-// function multiply(a, b) {
-//   return a * b;
-// }
-
-// // Division function
-// function divide(a, b) {
-//   if (b === 0) throw new Error("Cannot divide by zero");
-//   return a / b;
-// }
+// Division function
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero");
+  return a / b;
+}
 
 // Function to append value to the display
 function appendValue(value) {
