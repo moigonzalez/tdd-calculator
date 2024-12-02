6e3625e628ab8b5b75e4c139f9775ae2fa12c848
Moises <moises.gonzalez@flixbus.com>
2024-12-02 12:19:23 +0100

    Move tests

--- src/tests/display.test.js
+++ src/tests/display.test.js
@@ -4,7 +4,7 @@ const { JSDOM } = jsdom;
 const path = require('path');
 const html = path.join(__dirname, '../index.html');
 
-const { appendValue } = require('../js/index');
+const { appendValue, clearDisplay } = require('../js/index');
 
 describe('Calculator Display', () => {
   let display;
@@ -18,6 +18,31 @@ describe('Calculator Display', () => {
 
   test('must have a display to show the current input or result', () => {
     expect(display).toBeInTheDocument();
+  });
+
+  test('should clear the display', () => {
+    display.value = '123';
+    clearDisplay(display);
+    expect(display.value).toBe('');
+  });
+
+  test('should append values to the display', () => {
+    appendValue('1', display);
+    appendValue('+', display);
+    appendValue('2', display);
+    expect(display.value).toBe('1+2');
+  });
+
+  test('should delete the last character from the display', () => {
+    display.value = '123';
+    deleteLast(display);
+    expect(display.value).toBe('12');
+
+    deleteLast(display);
+    expect(display.value).toBe('1');
+
+    deleteLast(display);
+    expect(display.value).toBe(''); // Display is empty after deleting all characters
   });
 
   test('should should be read-only', () => {
